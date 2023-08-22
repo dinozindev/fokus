4983b22a3b8b2b530e8157f0a9a438b53ff0393e
dinozindev <pugplayzcraft@gmail.com>
2023-08-22 17:43:26 -0300

    adicionando estilizações para botão ativo, adicionando toggler de música com loop

--- script.js
+++ script.js
@@ -4,22 +4,41 @@ const curtoBtn = document.querySelector(".app__card-button--curto")
 const longoBtn = document.querySelector(".app__card-button--longo")
 const imgBanner = document.querySelector(".app__image")
 const titulo = document.querySelector(".app__title")
+const btns = document.querySelectorAll('.app__card-button')
+const musicToggleInput = document.getElementById("alternar-musica")
+const musica = new Audio("./sons/luna-rise-part-one.mp3");
+musica.loop = true;
+
+
+musicToggleInput.addEventListener("change", () => {
+    if(musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+})
 
 focoBtn.addEventListener("click", () => {
    alterarContexto('foco')
+   focoBtn.classList.add('active')
 })
 
 curtoBtn.addEventListener("click", () => {
    alterarContexto('descanso-curto')
+    curtoBtn.classList.add('active')
 })
 
 longoBtn.addEventListener("click", () => {
     alterarContexto('descanso-longo')
+    longoBtn.classList.add('active')
 })
 
 function alterarContexto(contexto) {
     html.setAttribute('data-contexto', contexto)
     imgBanner.setAttribute('src', `/imagens/${contexto}.png`)
+    btns.forEach(btn => {
+      btn.classList.remove('active')  
+    })
     switch (contexto) {
         case "foco":
             titulo.innerHTML = `
