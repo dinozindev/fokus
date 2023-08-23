306ca8f49e613513190be280704bda1d306d230a
dinozindev <pugplayzcraft@gmail.com>
2023-08-23 17:18:52 -0300

    Fim da aula 4. Adicionando a lógica da contagem regressiva (iniciar, pausar e finalizar tempo) + sons para cada situação

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ const longoBtn = document.querySelector(".app__card-button--longo")
 const imgBanner = document.querySelector(".app__image")
 const titulo = document.querySelector(".app__title")
 const btns = document.querySelectorAll('.app__card-button')
+const startPauseBtn = document.getElementById("start-pause")
 const musicToggleInput = document.getElementById("alternar-musica")
-const musica = new Audio("./sons/luna-rise-part-one.mp3");
-musica.loop = true;
 
+const musica = new Audio("./sons/luna-rise-part-one.mp3");
+const somPausar = new Audio("./sons/pause.mp3");
+const somTocar = new Audio("./sons/play.wav");
+const somFinalizar = new Audio("./sons/beep.mp3");
+
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musica.loop = true;
 
 musicToggleInput.addEventListener("change", () => {
     if(musica.paused) {
@@ -60,4 +69,39 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+// function a ser repetida, em que, a cada 1 seg, diminui 1 do valor de tempoDecorridoEmSegundos. Caso o valor chegue a 0, para a repetição e alerta que o tempo acabou. 
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        zerar()
+        somFinalizar.play();
+        alert("Tempo finalizado")
+        return;
+    } else {
+     tempoDecorridoEmSegundos--   
+    }
+    
+    console.log('Temporizador:' + tempoDecorridoEmSegundos, intervaloId)
+}
+
+// quando o botão começar/pausar for pressionado, invoca a function iniciarOuPausar
+startPauseBtn.addEventListener("click", iniciarOuPausar)
+
+// quando o botão começar for clicado, invoca a function iniciarOuPausar, em que, caso o intervaloId tenha valor, invoca a function zerar(), que pausará a repetição. Caso contrário, irá iniciar a repetição do setInterval, executando a function contagemRegressiva repetidamente. 
+function iniciarOuPausar() {
+    if(intervaloId) {
+        zerar()
+        somPausar.play()
+    } else {
+        somTocar.play()
+     intervaloId = setInterval(contagemRegressiva, 1000)   
+    }
+    
+}
+
+// quando o tempo decorrido chegar a zero ou o botão pausar for pressionado, invoca a function clearInterval, que irá cancelar a repetição do setInterval a partir de seu identificador intervaloId. 
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null;
 }
