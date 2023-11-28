bc276fd876821aa228b92fe515a0d5aa9858004b
dinozindev <pugplayzcraft@gmail.com>
2023-11-28 17:15:13 -0300

    finalizando aula 2. Adicionando aparencia das tarefas no site

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,10 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textArea = document.querySelector(".app__form-textarea");
+const listaTarefa = document.querySelector(".app__section-task-list")
 
-const tarefas = [];
+// se não houver nada na localStorage, retorna um array vazio. 
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
 function adicionarElementoTarefa(tarefa) {
     const li = document.createElement('li');
@@ -10,13 +12,32 @@ function adicionarElementoTarefa(tarefa) {
 
     const svg = document.createElement('svg');
     svg.innerHTML = `
-    <svg>
         <svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
+
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add("app__section-task-list-item-description")
+    paragrafo.textContent = tarefa.descricao;
+
+    const botao = document.createElement('button');
+    botao.classList.add("app_button-edit")
+
+    botao.onclick = () => {
+        prompt("Qual é o novo nome da tarefa?");
+    }
+    
+    const imagemBotao = document.createElement('img');
+    imagemBotao.setAttribute('src', './imagens/edit.png');
+    botao.append(imagemBotao);
+
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
+
+    return li;
 }
 
 btnAdicionarTarefa.addEventListener("click", () => {
@@ -29,5 +50,15 @@ formAdicionarTarefa.addEventListener("submit", (e) => {
         descricao: textArea.value
     }
     tarefas.push(tarefa);
+    const elementoTarefa = adicionarElementoTarefa(tarefa);
+    listaTarefa.append(elementoTarefa);
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+    textArea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
+})
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = adicionarElementoTarefa(tarefa);
+    listaTarefa.append(elementoTarefa);
 })
